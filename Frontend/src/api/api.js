a0b7b5deb6c3d95bd5bcb94e16188bc1167e0be8
Yalamanchili Rushi <rushiyalamanchili2@gmail.com>
2026-02-02 20:54:01 +0530

    added api

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -1,21 +1,15 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:import.meta.env.VITE_API_URL
+  baseURL: import.meta.env.VITE_API_URL
 });
 
-// 🔐 Attach token to every request
-API.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 export default API;
